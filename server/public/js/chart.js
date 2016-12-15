da70d834bbadad97fbb15faa4aeeb3b1dbfb98be
Andrew Azores <me@andrewazor.es>
2016-12-15 17:39:38 -0500

    MVC-ish refactor for chart.js

--- server/public/js/chart.js
+++ server/public/js/chart.js
@@ -1,33 +1,64 @@
-var core1Stats = ['core1'];
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      core1Stats
-    ],
-    types: {
-      core1: 'area'
+var cpuStatsModelPrototype = {
+    _data: [ 'core1' ],
+
+    init: function(){}
+
+e   setData: function(data) {
+        this._data.push(data.response[0].perProcessorUsage[0]);
+    },
+
+    update: function() {
+        _this = this;
+        $.ajax({
+            url: 'cpu-stats/latest',
+            success: data => { _this.setData(data); },
+            dataType: 'json',
+        });
     }
-  }
-});
+}
 
-var updateData = data => {
-  console.log(data);
-  core1Stats.push(data.response[0].perProcessorUsage[0]);
-  chart.load ({
-    columns: [
-      core1Stats
-    ]
-  });
-};
+var cpuStatsViewPrototype = {
+    chart: null,
 
-(function poll() {
-  setTimeout(function() {
-    $.ajax({
-      url: 'cpu-stats/latest',
-      success: updateData,
-      dataType: "json",
-      complete: poll
-    });
-  }, 1000);
-})();
+    init: function() {
+        this.chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [[]],
+                types: {
+                    core1: 'area'
+                }
+            },
+        });
+    },
+
+    setData: function(data) {
+        this.chart.load({
+            columns: [data]
+        });
+    }
+}
+
+var cpuStatsControllerPrototype = {
+    _model: null,
+    _view: null,
+
+    init: function() {
+        this._model.init();
+        this._view.init();
+    },
+
+    update: function() {
+        this._model.update();
+        this._view.setData(this._model._data);
+    },
+}
+
+var cpuStatsController = Object.create(cpuStatsControllerPrototype);
+cpuStatsController._view = Object.create(cpuStatsViewPrototype);
+cpuStatsController._model = Object.create(cpuStatsModelPrototype);
+
+cpuStatsController.init();
+setInterval(function() {
+    cpuStatsController.update();
+}, 1000);
