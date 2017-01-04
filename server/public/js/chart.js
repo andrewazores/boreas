9daf3c7d02f99b169ce1da8dbaad31bfc7942513
Andrew Azores <me@andrewazor.es>
2017-01-04 14:16:40 -0500

    Decouple controller and move to separate module

--- server/public/js/chart.js
+++ server/public/js/chart.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import ChartController from './controller.js';
+
 class CpuStatsModel {
     constructor() {
         this.data = [];
@@ -119,61 +121,7 @@ class CpuStatsView {
     }
 }
 
-class CpuStatsController {
-    constructor() {
-        this.model = new CpuStatsModel();
-        this.view = new CpuStatsView();
-        this.view.setPlaceholder();
-        this.enabled = true;
-        this.updatePeriod = 1000;
-        this.intervalId = null;
-
-        this.model.initKeys(keys => {
-            this.view.init(keys,
-                    (d, i) => { this.enabled = false; },
-                    (d, i) => { this.enabled = true; }
-                );
-        });
-    }
-
-    stop() {
-        if (this.intervalId) {
-            window.clearInterval(this.intervalId);
-            this.intervalId = null;
-        }
-    }
-
-    start() {
-        if (this.intervalId) {
-            stop();
-        }
-        if (this.updatePeriod <= 0) {
-            return;
-        }
-        this.intervalId = window.setInterval(() => { this.update(); }, this.updatePeriod);
-    }
-
-    setUpdatePeriod(period) {
-        this.updatePeriod = period;
-    }
-
-    update() {
-        this.model.update((keys, data) => {
-            if (!this.enabled) {
-                return;
-            }
-            this.view.setData([keys].concat(data));
-        });
-    }
-
-    setMaxAge(maxAge) {
-        this.model.maxAge = maxAge;
-        this.model.trim();
-        this.view.setData([this.model.keys].concat(this.model.data));
-    }
-}
-
-const cpuStatsController = new CpuStatsController();
+const cpuStatsController = new ChartController(new CpuStatsModel(), new CpuStatsView());
 
 function setUpdatePeriod(v) {
     cpuStatsController.stop.call(cpuStatsController);
---
+++ server/public/js/controller.js
@@ -0,0 +1,55 @@
+'use strict';
+
+export default class ChartController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+        this.view.setPlaceholder();
+        this.enabled = true;
+        this.updatePeriod = 1000;
+        this.intervalId = null;
+
+        this.model.initKeys(keys => {
+            this.view.init(keys,
+                    (d, i) => { this.enabled = false; },
+                    (d, i) => { this.enabled = true; }
+                );
+        });
+    }
+
+    stop() {
+        if (this.intervalId) {
+            window.clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    start() {
+        if (this.intervalId) {
+            stop();
+        }
+        if (this.updatePeriod <= 0) {
+            return;
+        }
+        this.intervalId = window.setInterval(() => { this.update(); }, this.updatePeriod);
+    }
+
+    setUpdatePeriod(period) {
+        this.updatePeriod = period;
+    }
+
+    update() {
+        this.model.update((keys, data) => {
+            if (!this.enabled) {
+                return;
+            }
+            this.view.setData([keys].concat(data));
+        });
+    }
+
+    setMaxAge(maxAge) {
+        this.model.maxAge = maxAge;
+        this.model.trim();
+        this.view.setData([this.model.keys].concat(this.model.data));
+    }
+}
